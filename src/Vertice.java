0f1a26083ea9e93f1e8a2fbb17f7b367abfd088f
RicardoReyes44 <rickyboy1002@gmail.com>
2020-11-26 22:15:26 -0600

    Se agregaron los metodos 'nomVertice', 'equals' y 'asigVert' en la clase 'Vertice'

--- src/Vertice.java
+++ src/Vertice.java
@@ -9,4 +9,18 @@ public class Vertice {
 	 numVertice = -1;
 	}
 	
+	public String nomVertice() // devuelve identificador del vértice
+	{
+	 return nombre;
+	}
+	
+	public boolean equals(Vertice n) // true, si dos vértices son iguales
+	{
+	 return nombre.equals(n.nombre);
+	}
+	
+	public void asigVert(int n) // establece el número de vértices
+	{
+	 numVertice = n;
+	}
 }
